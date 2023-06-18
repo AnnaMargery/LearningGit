d6154f5ca3a376b75465ea367bbf18f1b84748fd
Anna Margery <anna.szewc@gmail.com>
2023-06-18 23:37:44 +0200

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("To jest projekt do nauki pracy z gitem. Zmien cos prosze:)");
+    }
+}
