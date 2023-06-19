df247281f579f0248b1c6da3ceb31d2c035bb7b5
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2023-06-19 13:08:14 +0200

    another line added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,5 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("To jest projekt do nauki pracy z gitem. Zmien cos prosze:)");
+        System.out.println("Dodana linia");
     }
 }
